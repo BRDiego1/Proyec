4da9b44a9b94e6720f327cf1cbc2d3093dff0f61
BRDiego1 <nixonzapata507@gmail.com>
2025-02-23 00:51:36 -0600

    Implementacion y Desarrollo del proyecto

--- ASISMAY/src/main/java/com/home/asismay/HelloApplication.java
+++ ASISMAY/src/main/java/com/home/asismay/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Marina!");
+        stage.setTitle("Marina");
         stage.setScene(scene);
         stage.sizeToScene();
         stage.setResizable(true);
